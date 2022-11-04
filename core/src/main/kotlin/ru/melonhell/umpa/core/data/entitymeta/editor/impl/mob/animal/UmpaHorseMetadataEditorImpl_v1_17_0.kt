ceebb27c79eea07dadab0d3ead9c5e2cfe4e41d3
MelonHell <MelonHell@ya.ru>
2022-11-04 17:26:26 +0300

    fixed horse metadata

--- core/src/main/kotlin/ru/melonhell/umpa/core/data/entitymeta/editor/impl/mob/animal/UmpaHorseMetadataEditorImpl_v1_17_0.kt
+++ core/src/main/kotlin/ru/melonhell/umpa/core/data/entitymeta/editor/impl/mob/animal/UmpaHorseMetadataEditorImpl_v1_17_0.kt
@@ -5,14 +5,41 @@ import ru.melonhell.umpa.core.data.entitymeta.editor.base.mob.animal.UmpaHorseMe
 import ru.melonhell.umpa.core.data.entitymeta.editor.impl.abstr.UmpaAbstractHorseMetadataEditorImpl_v1_17_0
 import ru.melonhell.umpa.core.data.entitymeta.meta.mob.animal.UmpaHorseMetadata
 import ru.melonhell.umpa.core.wrappers.UmpaRawEntityMetadata
-import java.util.*
 
 @MinMaxMinecraftVersion("1.17", "latest")
 open class UmpaHorseMetadataEditorImpl_v1_17_0 : UmpaHorseMetadataEditor,
     UmpaAbstractHorseMetadataEditorImpl_v1_17_0() {
-    override fun readBaseColor(raw: UmpaRawEntityMetadata) = null // = raw.getValue(1, Color::class.java)
-    override fun writeBaseColor(raw: UmpaRawEntityMetadata, value: UmpaHorseMetadata.Color?) = Unit // = raw.setValue(1, value, Color::class.java)
+    private val colors = UmpaHorseMetadata.Color.values()
+    private val markings = UmpaHorseMetadata.Marking.values()
+    override fun readBaseColor(raw: UmpaRawEntityMetadata): UmpaHorseMetadata.Color? {
+        return readIntColor(raw)?.let { colors[it] }
+    }
 
-    override fun readMarkings(raw: UmpaRawEntityMetadata) = null // = raw.getValue(1, Marking::class.java)
-    override fun writeMarkings(raw: UmpaRawEntityMetadata, value: UmpaHorseMetadata.Marking?) = Unit // = raw.setValue(1, value, Marking::class.java)
+    override fun writeBaseColor(raw: UmpaRawEntityMetadata, value: UmpaHorseMetadata.Color?) {
+        if (value != null) writeColorAndMarking(raw, value.ordinal, readIntMarking(raw) ?: 0)
+    }
+
+    override fun readMarkings(raw: UmpaRawEntityMetadata): UmpaHorseMetadata.Marking? {
+        return readIntMarking(raw)?.let { markings[it] }
+    }
+
+    override fun writeMarkings(raw: UmpaRawEntityMetadata, value: UmpaHorseMetadata.Marking?) {
+        if (value != null) writeColorAndMarking(raw, readIntColor(raw) ?: 0, value.ordinal)
+    }
+
+    private fun readIntVariant(raw: UmpaRawEntityMetadata): Int? {
+        return raw.readValue(19, Int::class.java)
+    }
+
+    private fun readIntColor(raw: UmpaRawEntityMetadata): Int? {
+        return readIntVariant(raw)?.let { it and 255 }
+    }
+
+    private fun readIntMarking(raw: UmpaRawEntityMetadata): Int? {
+        return readIntVariant(raw)?.let { (it and '\uff00'.code) shr 8 }
+    }
+
+    private fun writeColorAndMarking(raw: UmpaRawEntityMetadata, color: Int, marking: Int) {
+        raw.writeValue(19, color and 255 or (marking shl 8 and '\uff00'.code), Int::class.java)
+    }
 }
